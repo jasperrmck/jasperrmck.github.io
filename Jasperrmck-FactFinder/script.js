5e9ceed420c157bb3549fe8f6865500652726abc
jasperrmck <86088421+jasperrmck@users.noreply.github.com>
2021-06-18 04:07:05 +0200

    Add files via upload

---
+++ Jasperrmck-FactFinder/script.js
@@ -0,0 +1,15 @@
+function f1(){
+	document.querySelector('#b').innerHTML = 'OMG usage can be traced back to 1917.<img src="images/omg.jpg" alt="omg grapics">';
+}
+function f2(){
+	document.querySelector('#b').innerHTML = 'New York was briefly named ~ New Orange ~.<img src="images/newyork.jpg" alt="NewYork picture">';
+}
+function f3(){
+	document.querySelector('#b').innerHTML = "Too much water can kill you.";
+}
+function f4(){
+	document.querySelector('#b').innerHTML = "The hottest temperature ever recorded on Earth was 2 billion degrees kelvin.";
+}
+function f5(){
+	document.querySelector('#b').innerHTML = "Hot water freezes faster than cold water.";
+}
